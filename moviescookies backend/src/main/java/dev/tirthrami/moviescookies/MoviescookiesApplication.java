a259d7b4cb3131bd4980c9c007d1a2785905cb16
tirthrami02 <104568327+tirthrami02@users.noreply.github.com>
2023-08-13 16:07:16 +0530

    initial

---
+++ moviescookies backend/src/main/java/dev/tirthrami/moviescookies/MoviescookiesApplication.java
@@ -0,0 +1,12 @@
+package dev.tirthrami.moviescookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviescookiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviescookiesApplication.class, args);
+	}
+}
